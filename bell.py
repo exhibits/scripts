daf914bfd9dbd1622017c865e2903544ad689040
Mike <godfreml@co.cal.md.us>
2015-08-28 10:46:26 -0400

    added bell script

---
+++ bell.py
@@ -0,0 +1,33 @@
+#!/usr/bin/
+
+######################################################
+# Michael Godfrey
+# 11/14/2014/
+# pushbutton player for Paleo Prep Lab
+# AUTORUN:
+# 	sudo nano ~/.config/lxsession/LXDE/autostart
+# 	make sure that the python call is given superuser
+######################################################
+import time
+import datetime
+import subprocess
+
+import RPi.GPIO as GPIO
+# Use Default Pin Numbers
+GPIO.setmode(GPIO.BOARD)
+# Disable warnings
+GPIO.setwarnings(False)
+# set pin 18 as input
+GPIO.setup(18, GPIO.IN)
+
+while True:	
+	if GPIO.input(18):
+
+		ts = time.time()
+		st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+		print("\nbutton pressed: ", st)
+
+		subprocess.call(["omxplayer", "/home/pi/DrumPointLighthouseBell3Strikes.wav"])
+	else:
+		time.sleep(.1)
+
