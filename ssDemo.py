7edd7b546de3bd71ee26675db91da060d19657f4
Michael Godfrey <vimgodfrey@gmail.com>
2015-09-14 05:20:54 +0000

    init commit

---
+++ ssDemo.py
@@ -0,0 +1,32 @@
+#!/usr/bin/
+
+######################################################
+# Michael Godfrey
+# 09/14/2015/
+# AUTORUN:
+######################################################
+import time
+import datetime
+import subprocess
+
+try:
+	import RPi.GPIO as GPIO
+except:
+	print "Error importing RPI.GPIO! Are you super? Has the module been installed?"
+
+# Use Default Pin Numbers
+GPIO.setmode(GPIO.BOARD)
+# Disable warnings
+#GPIO.setwarnings(False)
+# set pin 18 as input
+GPIO.setup(18, GPIO.IN)
+
+while True:	
+	if GPIO.input(18):
+
+		ts = time.time()
+
+		subprocess.call(["omxplayer", "/home/pi/DrumPointLighthouseBell3Strikes.wav"])
+	else:
+		time.sleep(.1)
+
