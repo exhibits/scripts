68186bced53018c5ff4a82323adcb2c7430cf47a
Mike <godfreml@co.cal.md.us>
2015-09-14 17:21:28 +0000

    gpio button push to make demo

---
+++ gpio12Test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/
+######################################################
+# Michael Godfrey
+# 9/14/2015
+######################################################
+import time
+import subprocess
+import sys
+
+import RPi.GPIO as GPIO
+# Use Default Pin Numbers
+GPIO.setmode(GPIO.BOARD)
+# Disable warnings
+GPIO.setwarnings(False)
+# Set pin 12 as Input - (GPIO18)
+GPIO.setup(12, GPIO.IN)
+
+while True:	
+	try:
+		if GPIO.input(12):
+			print("--Button Pressed--")
+			# Do stuff here. 
+			time.sleep(1)
+		else:
+			time.sleep(.1)
+
+	# End program cleanly with keyboard
+	except KeyboardInterrupt:
+		print "  Quit"
+		# Reset GPIO settings
+		GPIO.cleanup()
+		# Exit py script
+		sys.exit()
+
