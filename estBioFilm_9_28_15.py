977396844302eae330f23ac965a92042bd27e27b
Mike <godfreml@co.cal.md.us>
2015-09-28 20:12:21 +0000

    init live estBioFilmPlayer program

---
+++ estBioFilm_9_28_15.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+#######################################################################
+# Michael Godfrey
+# 9/14/2015
+#
+# Needs xdotool to capture mouse/touch input:
+# $ sudo apt-get install xdotool
+#
+# Make sure you have updated omxplayer:
+# $ sudo apt-get install omxplayer
+#
+# Loops a screen saver until MOUSE move, then plays film.
+#
+# *Remember* to set locations of desired files. See Below.
+#
+# Autorun on Desktop load:
+# $ sudo nano ~/.config/lxsession/LXDE/autostart
+# sudo ./home/pi/gitHub/exhibits/scripts/loopSSPushToPlayFilm.py
+#######################################################################
+import time
+import subprocess
+import sys
+import os
+#######################################################################
+# Set Correct File Locations HERE:
+#######################################################################
+screenSaverLocation 	= '/home/pi/slideShowIntro.mp4'
+filmLocation 		= 'main.mp4'
+bgLocation		= '/home/pi/blackBG.jpg'
+#######################################################################
+screenSaver = None
+# mouse x,y
+mouse = os.popen("xdotool getmouselocation")
+mouse = str(mouse.read())
+oMouse = None
+
+def playScreenSaver():
+	global screenSaver
+	screenSaver = subprocess.Popen(
+		['omxplayer', '--loop', '--blank', '--no-osd', screenSaverLocation], 
+		stdin=subprocess.PIPE, 
+		stdout=subprocess.PIPE, 
+		stderr=subprocess.STDOUT,
+		shell=False
+	)
+
+# MAIN
+# Fullscreen, HidePointer
+subprocess.Popen(['feh', '-F', bgLocation], shell=False) 
+# non-blocking
+playScreenSaver()
+
+while True:	
+	try:
+		oMouse = mouse
+		time.sleep(0.2)
+		mouse = os.popen("xdotool getmouselocation")
+		mouse = str(mouse.read())
+
+		if oMouse != mouse:
+			print('Killing Screen Saver.')
+			# Interact with process: Send data to stdin, returns tubple (stdoutdata, stderrdata)
+			screenSaver.communicate('q')
+			print('Loading Film...')
+			# OLD CALL... May need params to get audio working over HDMI for Screen.
+			#call(["omxplayer", "-o", "hdmi", "/home/pi/main.mp4"])
+			film = subprocess.Popen(
+				['omxplayer', filmLocation], 
+				stdin=subprocess.PIPE, 
+				stdout=subprocess.PIPE, 
+				stderr=subprocess.STDOUT,
+				shell=False
+			)
+			film.wait()
+			print("Film is OVER - Restarting ScreenSaver")
+			# reset mouse position
+			mouse = os.popen("xdotool getmouselocation")
+			mouse = str(mouse.read())
+			playScreenSaver()
+		else:
+			time.sleep(.1)
+	# End program cleanly with keyboard
+	except KeyboardInterrupt:
+		print "  Quit"
+		# Exit py script
+		sys.exit()
+
